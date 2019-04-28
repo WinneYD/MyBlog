1c521e2821a2585a9beb82cc24f6ce39a7f35c11
WWW <337530864@qq.com>
2019-04-28 21:15:22 +0800

    fix: 修复注册不成功和登录跳转

--- wydblog/account/views.py
+++ wydblog/account/views.py
@@ -4,20 +4,20 @@ from .forms import RegistrationForm,UserProfileForm
 def register(request):
     if request.method == 'POST':
         user_form=RegistrationForm(request.POST)
-        userprofile_form=UserProfileForm(request.POST)
-        if user_form.is_valid()*userprofile_form.is_valid():
+        #userprofile_form=UserProfileForm(request.POST)
+        if user_form.is_valid(): #*userprofile_form.is_valid():
             new_user=user_form.save(commit=False)
             new_user.set_password(user_form.cleaned_data['password'])
             new_user.save()
-            new_profile=userprofile_form.save(commit=False)
-            new_profile.user=new_user
-            new_profile.save()
+            #new_profile=userprofile_form.save(commit=False)
+            #new_profile.user=new_user
+           # new_profile.save()
             return HttpResponse('successful')
         else:
             return HttpResponse('sorry')
     else:
         user_form=RegistrationForm()
-        userprofile_form=UserProfileForm()
+        #userprofile_form=UserProfileForm()
         return render(request,'account/register.html',{'form':user_form})
 
 
